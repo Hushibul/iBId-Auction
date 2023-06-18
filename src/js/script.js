fa0a9182b60ef7ef7b4a9f6d19ce34f4c0bebf4b
Hushibul <nayeemninz786@gmail.com>
2023-06-18 19:58:54 +0600

    card gallery completed mobile nav added latest news added

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,35 @@
-const menuList = document.querySelector(".menu-list");
-const listOne = document.querySelector("list-one");
-const galleryMenu = document.querySelector("#gallery-menu");
+const openNav = document.querySelector("#open-nav");
+const moblieNav = document.querySelector("#mobile-nav");
+const body = document.querySelector("body");
 
-const galleryChild = galleryMenu.childNodes;
+openNav.addEventListener("click", function () {
+  moblieNav.classList.replace("hidden", "block");
 
-function toggleList() {
-  for (let i = 1; i < galleryChild.length; i = i + 2) {
-    galleryChild[i].classList.replace("hidden", "block");
-  }
-}
+  const closeMenu = document.createElement("div");
+  closeMenu.setAttribute(
+    "class",
+    "w-full h-screen bg-black absolute inset-0 opacity-20"
+  );
+  body.appendChild(closeMenu);
+  closeMenu.addEventListener("click", function () {
+    moblieNav.classList.replace("block", "hidden");
+    closeMenu.setAttribute("class", "hidden");
+  });
+});
+
+const openFilter = document.querySelector("#open-filter");
+const closeFilter = document.querySelector("#close-filter");
+const cardFilter = document.querySelector("#card-filter");
+const cardsGallery = document.querySelector("#cards-gallery");
+
+closeFilter.addEventListener("click", function () {
+  cardFilter.classList.replace("lg:block", "lg:hidden");
+  cardsGallery.classList.replace("lg:w-4/5", "lg:w-full");
+  closeFilter.classList.add("hidden");
+});
+
+openFilter.addEventListener("click", function () {
+  cardFilter.classList.replace("lg:hidden", "lg:block");
+  closeFilter.classList.replace("hidden", "block");
+  cardsGallery.classList.replace("lg:w-full", "lg:w-4/5");
+});
