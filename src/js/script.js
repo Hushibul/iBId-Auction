efc3f7f908faf6a8a7379495187dcad86a30223b
Hushibul <nayeemninz786@gmail.com>
2023-06-19 19:56:40 +0600

    sales banner added mixitup jquery library implemented

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Navmenu Manupulation
 const openNav = document.querySelector("#open-nav");
 const moblieNav = document.querySelector("#mobile-nav");
 const body = document.querySelector("body");
@@ -17,6 +18,7 @@ openNav.addEventListener("click", function () {
   });
 });
 
+//Filter Menu Manupulation
 const openFilter = document.querySelector("#open-filter");
 const closeFilter = document.querySelector("#close-filter");
 const cardFilter = document.querySelector("#card-filter");
@@ -32,4 +34,33 @@ openFilter.addEventListener("click", function () {
   cardFilter.classList.replace("lg:hidden", "lg:block");
   closeFilter.classList.replace("hidden", "block");
   cardsGallery.classList.replace("lg:w-full", "lg:w-4/5");
+});
+
+//Mixitup
+let mixer = mixitup(cardsGallery, {
+  selectors: {
+    target: ".mix",
+  },
+});
+
+let filterButton = document.querySelectorAll(".filter");
+filterButton.forEach(function (button) {
+  button.addEventListener("click", function () {
+    let filterValue = button.getAttribute("data-filter");
+    mixer.filter(filterValue);
+  });
+});
+
+console.log(cardsGallery.childNodes[1].childNodes[3].textContent);
+
+//Search Filter
+const searchFilter = document.querySelector("#search-filter");
+searchFilter.addEventListener("keyup", function (event) {
+  const searchTerm = searchFilter.value.trim().toLowerCase();
+  console.log(searchTerm);
+  if (searchTerm !== "") {
+    mixer.filter(function (item) {});
+  } else {
+    mixer.filter("all");
+  }
 });
