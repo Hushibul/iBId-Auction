5e9755c7318f4d0086550cdabc87de1d4a10844d
Hushibul <nayeemninz786@gmail.com>
2023-06-21 16:04:02 +0600

    Animation applied & minor bug fix

--- src/js/script.js
+++ src/js/script.js
@@ -18,13 +18,35 @@ openNav.addEventListener("click", function () {
   });
 });
 
+//Select Category
+const selectCategory = document.querySelector("#select-category");
+const selectOptions = document.querySelector(".options");
+const listOptions = document.querySelectorAll(".list-options");
+
+selectCategory.addEventListener("click", function () {
+  selectOptions.classList.remove("hidden");
+
+  listOptions.forEach(function (element) {
+    element.addEventListener("click", function () {
+      selectCategory.textContent = element.textContent;
+      selectOptions.classList.add("hidden");
+    });
+  });
+});
+
 //Mouse Movement Hero Animation
 const hero = document.querySelector("#hero");
+const heroShadow = document.querySelectorAll(".hero-shadow");
 hero.addEventListener("mousemove", function (event) {
   const x = event.clientX;
-  const y = event.clientY;
 
-  console.log(x, y);
+  const innerWidth = window.innerWidth;
+
+  console.log("hero", hero.offsetLeft, hero.offsetTop);
+
+  heroShadow.forEach(function (element) {
+    console.log(element.offsetTop, element.offsetLeft);
+  });
 });
 
 //Filter Menu Manupulation
@@ -45,6 +67,22 @@ openFilter.addEventListener("click", function () {
   cardsGallery.classList.replace("lg:w-full", "lg:w-4/5");
 });
 
+//Filter Dropdown
+const filterMenu = document.querySelector("#filter-menu");
+const filterElement = document.querySelectorAll(".menu-list");
+const filterToggle = document.querySelector("#filter-list-toggle");
+
+filterToggle.addEventListener("click", function () {
+  filterMenu.classList.replace("hidden", "flex");
+  filterElement.forEach(function (element) {
+    element.addEventListener("click", function () {
+      filterToggle.textContent = element.textContent;
+
+      filterMenu.classList.replace("flex", "hidden");
+    });
+  });
+});
+
 //Mixitup
 let mixer = mixitup(cardsGallery, {
   selectors: {
@@ -61,17 +99,17 @@ searchFilter.addEventListener("keyup", function (event) {
   let matchingElements = [];
   const searchTerm = searchFilter.value.trim().toLowerCase();
   if (searchTerm !== "") {
-    var mixElements = document.querySelectorAll(".mix");
+    let mixElements = document.querySelectorAll(".mix");
 
     mixElements.forEach(function (element) {
-      var title = element.querySelector("h3").textContent.toLowerCase();
+      let title = element.querySelector("h3").textContent.toLowerCase();
 
       // Add item to be filtered out if input text matches items inside the title
       if (title.includes(searchTerm)) {
         matchingElements.push(element);
       } else {
         // Remove any previously matched item
-        var index = matchingElements.indexOf(element);
+        let index = matchingElements.indexOf(element);
         if (index > -1) {
           matchingElements.splice(index, 1);
         }
